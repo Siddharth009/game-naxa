2fced672d41581f8e8b165adf51a613d80e4171a
Sid <spaudel206@gmail.com>
2019-06-09 19:08:19 +0545

    Final  Commit

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
 
-var scores, roundscore, activeplayer;
-
-scores = [0,0];
-roundscore = 0;
-activeplayer = 0;
-
-
-
-
-
-
-
-
-
-document.getElementById('dice-1').style = "display:none";
-
-window.onload=function(){
-    document.getElementById('btn-roll').addEventListener('click', function() {
-
-        var dice = Math.floor(Math.random()*6)+1;
-        document.getElementById("current-0").innerHTML = dice;
-    
-    })
-
-  }
-
 
 
 
